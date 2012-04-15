1b3ce7e6c46b6792bb6e160b5f0e471037faf515
David Whitney <dwhitnee@gmail.com>
2012-04-14 23:48:52 -0700

    toggle layout editablility

--- layout/grid.js
+++ layout/grid.js
@@ -1,10 +1,28 @@
 $(document).ready(
     function(){
-        $('#dashboard-layout .sortable-list').sortable(
+        $('#dashboard-layout .widget-list').sortable(
             {
-                connectWith: '#dashboard-layout .sortable-list',
-                placeholder: 'placeholder'
+                connectWith: '#dashboard-layout .widget-list',
+                placeholder: 'placeholder',
+                forcePlaceholderSize: true
+                // grid: [50, 20] 
             }
         );
+        
+        $("#edit-layout").click(
+            function(e) {
+                var $layout = $("#dashboard-layout .widget-list");
+                var disabled = $layout.sortable( "option", "disabled" );
+                $layout.sortable( "option", "disabled", !disabled );
+
+                if (disabled) {
+                    $(this).text("Done");
+                } else {
+                    $(this).text("Edit");
+                }
+            });
     }
+
+
+
 );
